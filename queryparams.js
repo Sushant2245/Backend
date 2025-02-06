8c101500b3ad4807b9f521800d18df1571d018cd
Sushant2245 <guptasushant426@gmail.com>
2025-02-06 19:29:42 +0530

    Add files via upload

---
+++ queryparams.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+// app.get('/profile', (req, res) => {
+//     const {username,age} = req.query;
+//     console.log(username);
+//     res.send("Hello from the profile page" + " " + username + " " + age);
+// })
+
+app.get("/profile/:id/:username", (req, res) =>{
+    const {id,username} = req.params;
+    res.send(`Profile page with id: ${id} & username: ${username}`);
+})
+
+app.listen(3477, (req, res)=>{
+    console.log("server started");
+});
